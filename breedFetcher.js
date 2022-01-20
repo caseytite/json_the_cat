420cbb0af2f1a1fcc2f47423184da451cd8b1f35
Casey <caseyjtite@gmail.com>
2022-01-20 14:37:55 -0800

    refactor breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,26 @@
 const request = require("request");
-const breed = process.argv[2];
+// const breed = process.argv[2];-------------- moved to index
 // const quality = process.argv[3];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, response, body) => {
-    const data = JSON.parse(body)[0];
-    if (error) {
-      console.log("error:", "Improper Breed or Information request"); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      return;
+const fetchBreedDescription = function (breed, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+    (error, response, body) => {
+      console.log("in callback");
+      const data = JSON.parse(body)[0];
+      if (error) {
+        callback(error, null);
+        // console.log("error:", "Improper Breed or Information request"); // Print the error if one occurred
+        // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+        return;
+      }
+      if (data === undefined) {
+        console.log("Breed not found");
+        return;
+      }
+      callback(null, data.description);
     }
-    if (data === undefined) {
-      console.log("Breed not found");
-      return;
-    }
-    // console.log(data);
-    console.log("Cat breed:", data["description"]);
-    // console.log("Searched Info -", quality, ":", data[0][quality]); // Print the HTML for the Google homepage.
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchBreedDescription } = require("./breedFetcher");
+
+const breed = process.argv[2];
+
+fetchBreedDescription(breed, (error, desc) => {
+  if (error) {
+    console.log("Error fetching details", error);
+  } else {
+    console.log(desc);
+    console.log("Cat breed:", desc);
+  }
+});
